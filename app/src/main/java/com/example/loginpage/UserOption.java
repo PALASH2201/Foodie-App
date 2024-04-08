de9ccb339e881c0c84410e1cd64db49b545b5e51
Aadit-001 <developer.aj_001@outlook.com>
2024-04-09 02:12:56 +0530

    Register page design

--- app/src/main/java/com/example/loginpage/UserOption.java
+++ app/src/main/java/com/example/loginpage/UserOption.java
@@ -29,7 +29,7 @@ public class UserOption extends AppCompatActivity {
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.orange));
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
         }
 
 
