e7287665a851c4c501c92e76a5a26335d83c2a1c
PALASH2201 <palashmahajan22@gmail.com>
2024-04-09 17:38:13 +0530

    Add to Cart functionality added

--- app/src/main/java/com/example/loginpage/CartDataClass.java
+++ app/src/main/java/com/example/loginpage/CartDataClass.java
@@ -1,57 +1,35 @@
 package com.example.loginpage;
 
-public class DishDataClass {
+public class CartDataClass {
 
     private String dish_name ;
-    private String dish_description ;
     private String dish_price;
-    private String dish_rating;
     private String dish_image_url ;
     private String restaurant_id;
     private String category_id;
     private String key;
+
     private int quantity;
 
-    public DishDataClass(String dish_name, String dish_description, String dish_price,String dish_image_url, String restaurant_id, String category_id , String key) {
+    public CartDataClass(String dish_name, String dish_price, String dish_image_url, String restaurant_id, String category_id, String key, int quantity) {
         this.dish_name = dish_name;
-        this.dish_description = dish_description;
         this.dish_price = dish_price;
         this.dish_image_url = dish_image_url;
         this.restaurant_id = restaurant_id;
         this.category_id = category_id;
         this.key = key;
-        this.quantity = 1;
+        this.quantity = quantity;
     }
 
-    public void setDish_rating(String dish_rating) {
-        this.dish_rating = dish_rating;
-    }
-
-    public DishDataClass() {
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public int getQuantity() {
-        return quantity;
+    public CartDataClass() {
     }
 
     public String getDish_name() {
         return dish_name;
     }
 
-    public String getDish_description() {
-        return dish_description;
-    }
-
     public String getDish_price() {
         return dish_price;
-    }
-
-    public String getDish_rating() {
-        return dish_rating;
     }
 
     public String getDish_image_url() {
@@ -64,5 +42,17 @@ public class DishDataClass {
 
     public String getCategory_id() {
         return category_id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return Double.parseDouble(dish_price)*quantity;
     }
 }
--- app/src/main/java/com/example/loginpage/User_cart.java
+++ app/src/main/java/com/example/loginpage/User_cart.java
@@ -1,14 +1,47 @@
 package com.example.loginpage;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class User_cart extends AppCompatActivity {
 
+    private Cart_myAdapter cartMyAdapter;
+    private AlertDialog dialog;
+    List<CartDataClass> dataList ;
+
+    String extra_restaurant_name , extra_category_name , extra_restaurant_id , extra_category_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_cart);
+
+        Intent intent = getIntent();
+        extra_restaurant_name = intent.getStringExtra("Restaurant Name");
+        extra_category_name = intent.getStringExtra("Category Name");
+        extra_restaurant_id = intent.getStringExtra("Restaurant Id");
+        extra_category_id = intent.getStringExtra("Category Id");
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(User_cart.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        dialog = builder.create();
+        dialog.show();
+
+        dataList = new ArrayList<>();
+        cartMyAdapter = new Cart_myAdapter(User_cart.this , dataList);
+        recyclerView.setAdapter(cartMyAdapter);
+
+
     }
 }
